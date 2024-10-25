7b2be7a04ba37cfae30b865bb0eba69ceaedcc51
katherinheb <katherinheb@gmail.com>
2024-10-25 11:05:04 -0400

    Add files via upload

---
+++ index.mjs
@@ -0,0 +1,7 @@
+
+import message from "./main.mjs";
+
+console.log(message());
+
+// console.log(dogBreed,dogName, dogsAge);
+
---
+++ main.mjs
@@ -0,0 +1,8 @@
+const message = () => {
+  const dogBreed = "Huskimo"
+  const dogName = "Emma";
+  const dogsAge = 4;
+  return dogName + ' is a ' + dogBreed + ' and is ' + dogsAge + ' years old. ';
+};
+
+export default message;
